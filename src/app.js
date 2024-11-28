2af3192c87b6af7b3a36f9574ad645cc4b944b07
franlopezperez <154910836+franlopezperez@users.noreply.github.com>
2024-11-28 12:32:17 +0000

    cambios generador de cartas

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const symbols = ["♦", "♥", "♠", "♣"];
+  const numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+
+  function createCard() {
+    const randomSymbol = symbols[Math.floor(Math.random() * symbols.length)];
+    const randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
+
+    document.querySelector(".top-left").textContent = randomSymbol;
+    document.querySelector(".body").textContent = randomNumber;
+    document.querySelector(".bottom-right").textContent = randomSymbol;
+
+    const card = document.querySelector(".card");
+    if (randomSymbol === "♥" || randomSymbol === "♦") {
+      card.classList.remove("text-dark");
+      card.classList.add("text-danger");
+    } else {
+      card.classList.remove("text-danger");
+      card.classList.add("text-dark");
+    }
+  }
+
+  createCard();
+
+  document.getElementById("generate").addEventListener("click", createCard);
 };
